6f2dd0762079c7c831728e0b0f3b7f974be1763e
Shazil Raza <shazilraza8@gmail.com>
2024-11-23 15:56:28 +0530

    Updates

--- src/components/GeneralButton.jsx
+++
@@ -1,4 +0,0 @@
-function GeneralButton({ styleObject, text }) {
-  return <button className={`h-14 w-60 hover:border-2 hover:border-white`} style={styleObject}>{text}</button>;
-}
-export default GeneralButton;
--- src/components/GeneralContainer.jsx
+++
@@ -1,5 +0,0 @@
-function GeneralContainer( {children,styleObject }) {
-  return <div style={styleObject}>{children}</div>;
-}
-
-export default GeneralContainer;
